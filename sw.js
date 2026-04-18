56f77bbc9d46e46058b58f3abcdc34e9ea24dfbf
Borislav Bošnjak <pietas.pula@gmail.com>
2026-04-18 22:49:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // FotoKalk Service Worker — v1.0
 // Cache-first strategija za offline upotrebu
 
-const CACHE_NAME = 'fotokalkpro-v1.1.0.';
+const CACHE_NAME = 'fotokalkpro-v1.1.1.';
 const ASSETS = [
   './index.html',
   './manifest.json',
